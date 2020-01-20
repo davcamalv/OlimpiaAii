920c56f786705d5c2c054c0700e1f410178825cb
Antonio <antcarluq@alum.us.es>
2020-01-20 12:56:12 +0100

    feat (modelo): Se ha creado el modelo de datos
    Se ha creado el modelo de datos con las entidades, sus relaciones, sus atributos y sus restricciones.
    
    Closed #1

---
+++ BiKlopp/BiKlopp/models.py
@@ -0,0 +1,67 @@
+# encoding:utf-8
+
+from django.db import models
+from django.core.validators import MinValueValidator
+
+class Equipo(models.Model):
+    id_equipo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    foto = models.URLField()
+    victorias = models.PositiveIntegerField()
+    derrotas = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ('nombre',)
+
+class Jugador(models.Model):
+    id_jugador = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    foto = models.URLField()
+    POSICIONES = (('PT', 'Portero'), ('DF', 'Defensa'), ('MC', 'Mediocentro'), ('DL', 'Delantero'))
+    posicion = models.CharField(choices=POSICIONES)
+    forma = models.CharField(max_length=50)
+    ultimos_puntos = models.TextField(max_length=200)
+    puntos_totales = models.IntegerField()
+    valor_mercado = models.IntegerField(MinValueValidator=150000)
+    partidos_jugados = models.PositiveIntegerField()
+    goles = models.PositiveIntegerField()
+    tarjetas = models.PositiveIntegerField()
+    media_puntos = models.FloatField()
+    id_equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    id_mi_equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    id_mercado = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ('nombre',)
+
+
+
+class MiEquipo(models.Model):
+    id_mi_equipo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    ultima_fecha_actualizacion = models.DateTimeField(auto_now=True) #Se actualiza automaticamente cada save()
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ('nombre',)
+
+
+class Mercado(models.Model):
+    id_mercado = models.AutoField(primary_key=True)
+    ultima_fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id_mercado
+
+    class Meta:
+        ordering = ('id_mercado',)
+
+
