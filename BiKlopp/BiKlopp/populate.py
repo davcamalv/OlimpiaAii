ef33c4c888a1a5dc2f2ff9cd974335ec8b985d66
Antonio <antcarluq@alum.us.es>
2020-01-21 10:43:01 +0100

    Auto stash before merge of "feature/populate" and "origin/feature/populate"

--- BiKlopp/BiKlopp/populate.py
+++ BiKlopp/BiKlopp/populate.py
@@ -8,14 +8,16 @@ import os
 from django.core.exceptions import ObjectDoesNotExist
 from BiKlopp.models import Equipo, Jugador, Mercado
 from django.conf import settings
+
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 
+
 def popular_jugadores_mercado(usuario, contrasena):
     path = os.getcwd()
-    url = os.path.join(path, "BiKlopp/resources/chromedriver.exe").replace("\\","/")
-   
+    url = os.path.join(path, "BiKlopp/resources/chromedriver.exe").replace("\\", "/")
+
     driver = webdriver.Chrome(url, options=options)
 
     driver.get('https://biwenger.as.com/login')
@@ -23,26 +25,28 @@ def popular_jugadores_mercado(usuario, contrasena):
 
     username = driver.find_element_by_name('email')
     username.send_keys(usuario)
-    
+
     password = driver.find_element_by_name('password')
     password.send_keys(contrasena)
 
     driver.find_element_by_class_name('btn.squared.success').click()
 
-    #Espera para completar el logeo y poder acceder al mercado
+    # Espera para completar el logeo y poder acceder al mercado
     time.sleep(2)
 
     driver.get('https://biwenger.as.com/market')
 
-    #Espera para cargar el mercado y poder acceder a los jugadores
+    # Espera para cargar el mercado y poder acceder a los jugadores
     time.sleep(2)
-   
+
     mercado = Mercado.objects.create()
     i = 0
     jugadores = driver.find_elements_by_tag_name("player-card")
     urls = []
     for jugador in jugadores:
-        urls.append(str(jugador.find_element_by_class_name("content").find_element_by_class_name("main").find_element_by_tag_name("h3").find_element_by_tag_name("a").get_attribute("href")))
+        urls.append(str(
+            jugador.find_element_by_class_name("content").find_element_by_class_name("main").find_element_by_tag_name(
+                "h3").find_element_by_tag_name("a").get_attribute("href")))
 
     while i < 1:
 
@@ -50,13 +54,13 @@ def popular_jugadores_mercado(usuario, contrasena):
 
         driver.get(url)
 
-        #Espera para cargar el jugador y poder extraer los datos
+        # Espera para cargar el jugador y poder extraer los datos
         time.sleep(2)
 
         page = BeautifulSoup(driver.page_source, "html5lib")
-        
-        nombre = page.find("h1",{"itemprop": "name"}).getText()
-        foto = page.find("img",{"itemprop": "image"})["src"]
+
+        nombre = page.find("h1", {"itemprop": "name"}).getText()
+        foto = page.find("img", {"itemprop": "image"})["src"]
         forma_fisica = page.find('player-status')["title"]
         posicion = page.find('player-position').getText()
         puntos = []
@@ -79,7 +83,7 @@ def popular_jugadores_mercado(usuario, contrasena):
         media_puntos =  estadisticas[6].find("div", {"class": "stat main"}).find("span").getText().replace(",", ".")
         
         driver.get(url_completa_equipo)
-        #Espera para cargar el equipo y poder extraer los datos
+        # Espera para cargar el equipo y poder extraer los datos
         time.sleep(2)
         page = BeautifulSoup(driver.page_source, "html5lib")
 
@@ -91,7 +95,8 @@ def popular_jugadores_mercado(usuario, contrasena):
             Equipo.objects.filter(nombre=nombre_equipo).update(nombre=nombre_equipo,foto=foto_equipo, victorias=victorias_equipo, derrotas=derrotas_equipo)
             equipo = equipo_bd
         except ObjectDoesNotExist:
-            nuevo_Equipo = Equipo(nombre=nombre_equipo,foto=foto_equipo, victorias=victorias_equipo, derrotas=derrotas_equipo)
+            nuevo_Equipo = Equipo(nombre=nombre_equipo, foto=foto_equipo, victorias=victorias_equipo,
+                                  derrotas=derrotas_equipo)
             nuevo_Equipo.save()
             equipo = nuevo_Equipo
 
@@ -100,8 +105,7 @@ def popular_jugadores_mercado(usuario, contrasena):
         except ObjectDoesNotExist:
             nuevo_jugador = Jugador(nombre=nombre,foto=foto, posicion= posicion, forma=forma_fisica, ultimos_puntos=puntos, puntos_totales=int(puntos_totales), valor_mercado=int(valor), partidos_jugados=int(partidos_jugados), goles=int(goles), tarjetas=int(tarjetas), media_puntos=float(media_puntos), id_equipo=equipo, id_mercado=mercado)
             nuevo_jugador.save()
-           
+
         i = i + 1
 
     driver.quit()
-
--- BiKlopp/BiKlopp/views.py
+++ BiKlopp/BiKlopp/views.py
@@ -1,5 +1,7 @@
 from BiKlopp.populate import popular_jugadores_mercado
 from BiKlopp.models import Equipo, Jugador, Mercado
+from django.http import HttpResponseRedirect
 
 def popularJugadoresMercado(request):
-    popular_jugadores_mercado("dcamalv@gmail.com", "contraseña") 
+    popular_jugadores_mercado("dcamalv@gmail.com", "contraseña")
+    return HttpResponseRedirect('/admin/')
